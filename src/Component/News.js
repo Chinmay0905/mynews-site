dabb2f660cfceef298e1574b71e19dcfb85119ba
Chinmay <chinmaybhargava95022002@gmail.com>
2022-07-16 05:59:41 +0530

    changed heading's color

--- src/Component/News.js
+++ src/Component/News.js
@@ -52,7 +52,7 @@ const News = (props) => {
 
         return (
             <>
-                <h1 className="text-center" style={{marginTop:'56px' ,color:'white'}}>DG News - Top headlines on {capitalizeFirstLetter(props.category)}</h1>
+                <h1 className="text-center" style={{marginTop:'84px' ,color:'black'}}>DG News - Top headlines on {capitalizeFirstLetter(props.category)}</h1>
                 {loading && <Spinner/>}
                 <InfiniteScroll
                     dataLength={articles.length}
